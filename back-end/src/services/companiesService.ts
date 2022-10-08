008092a9ef5d26cd2463b1c1b4c317aac6d62353
alyson <consulialyson@gmail.com>
2022-10-07 22:22:11 -0300

    reafactor: companies service

--- back-end/src/services/companiesService.ts
+++ back-end/src/services/companiesService.ts
@@ -1,10 +1,10 @@
+import { Companies } from "@prisma/client";
 import { CompanyInsertData } from "../interfaces/createData.js";
-import {
-  conflictError,
-  notFoundError,
-  unprocessableEntityError,
-} from "../middlewares/errorHandlingMiddleware.js";
 import { companiesRepository } from "../repositories/companiesRepository.js";
+import {
+  __validateIdOrFail,
+  __validateNameOrFail,
+} from "../utils/validateData.js";
 
 const getCompanies = async () => {
   const companies = await companiesRepository.findMany();
@@ -13,37 +13,28 @@ const getCompanies = async () => {
 
 const registerCompany = async (company: CompanyInsertData) => {
   const { name } = company;
-  await __validateNameOrFail(name);
+  await __validateNameOrFail<Companies>(name, "companies", "Company");
   await companiesRepository.insert(company);
 };
 
 const updateCompany = async (company: CompanyInsertData, companyId: string) => {
   const { name } = company;
-  const selectedCompany = await __validateIdOrFail(companyId);
+  const selectedCompany = await __validateIdOrFail<Companies>(
+    companyId,
+    "companies",
+    "Company",
+  );
 
   if (name !== selectedCompany.name) {
-    await __validateNameOrFail(name);
+    await __validateNameOrFail<Companies>(name, "companies", "Company");
   }
 
   await companiesRepository.update(companyId, company);
 };
 
 const deleteCompany = async (companyId: string) => {
-  await __validateIdOrFail(companyId);
+  await __validateIdOrFail<Companies>(companyId, "companies", "Company");
   await companiesRepository.deleteById(companyId);
-};
-
-const __validateNameOrFail = async (name: string) => {
-  const company = await companiesRepository.findByName(name);
-  if (company) throw conflictError("Company name already exists!");
-};
-
-const __validateIdOrFail = async (id: string) => {
-  if (id.length !== 24)
-    throw unprocessableEntityError("Company id must be exactly 24 char!");
-  const company = await companiesRepository.findById(id);
-  if (!company) throw notFoundError("Company not found!");
-  return company;
 };
 
 export const companiesService = {
