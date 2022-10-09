42cb5b94e7ce83f2b98c3f310b3e38d1e1991c91
alyson <consulialyson@gmail.com>
2022-10-08 21:31:21 -0300

    feat: router to validate token

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,14 +2,13 @@ import { Router } from "express";
 
 import { authController } from "../controllers/authController.js";
 import { validateSchema } from "../middlewares/schemaValidator.js";
+import { validateToken } from "../middlewares/validateToken.js";
 import { signinSchema } from "../schemas/authSchema.js";
 
 const authRouter = Router();
 
-authRouter.post(
-  "/sign-in",
-  validateSchema(signinSchema),
-  authController.signin,
-);
+authRouter
+  .post("/sign-in", validateSchema(signinSchema), authController.signin)
+  .post("/validate-token", validateToken, (req, res) => res.sendStatus(200));
 
 export default authRouter;
